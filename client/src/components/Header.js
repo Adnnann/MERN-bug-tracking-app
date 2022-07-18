d85c7b960fab222f43c50261a444b68b55ba0ab9
Adnnann <adnanovcina1987@gmail.com>
2022-07-18 23:40:23 +0200

    revised

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { AppBar } from "@mui/material";
-import { Toolbar } from "@mui/material";
+
 import Box from "@mui/material/Box";
 import bugTrackerIcon from "../assets/images/bugTrackerIcon.jpeg";
-import { Grid } from "@mui/material";
+
 import Logout from "./Logout";
-import DashboardButtons from "./DashboardButtons";
+
+import { getUserSigninData } from "../features/bugsSlice";
 
 const Header = () => {
+  const userData = useSelector(getUserSigninData);
+  console.log(userData);
   return (
     //set margin on body tag to 0 to prevent AppBar to add margin on screen
     <AppBar position="static">
@@ -17,11 +21,14 @@ const Header = () => {
         marginTop="20px !important"
         margin="0 auto"
         src={bugTrackerIcon}
+        marginBottom={window.location.pathname === "/" ? "20px" : "0px"}
       ></Box>
 
-      <div style={{ marginLeft: "auto" }}>
-        <Logout />
-      </div>
+      {userData?.user && (
+        <div style={{ marginLeft: "auto" }}>
+          <Logout />
+        </div>
+      )}
     </AppBar>
   );
 };
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,132 +1,153 @@
-import Grid from '@mui/material/Grid'
-import Item from '@mui/material/Grid'
-import TextField from '@mui/material/TextField';
-import { CardActions, Card, CardContent, Button, Typography, Icon } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import { useState } from 'react';
-import {useSelector, useDispatch} from 'react-redux'
-import {useNavigate} from 'react-router-dom'
-import { getToken, 
-        getUserSigninData, 
-        loginUser, 
-        resetStore
-} from '../features/bugsSlice';
-import { useEffect } from 'react';
+import Grid from "@mui/material/Grid";
+import Item from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import {
+  CardActions,
+  Card,
+  CardContent,
+  Button,
+  Typography,
+  Icon,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  getToken,
+  getUserSigninData,
+  loginUser,
+  resetStore,
+} from "../features/bugsSlice";
+import { useEffect } from "react";
 
 const useStyles = makeStyles({
-    card: {
-        maxWidth: 600,
-        margin: 'auto',
-        textAlign: 'center',
-        marginTop: '30px',
-        paddingBottom: '20px'
-    },
-    error: {
-        verticalAlign: 'middle'
-    },
-    title:{
-        marginTop: '20px',
-        color: 'red'
-    },
-    textField:{
-        marginLeft: '10px',
-        marginRight: '10px',
-        width: 300
-    },
-    submit:{
-        alignSelf:'flex-end',
-        margin: 'auto',
-        marginBottom: '10px',
-        minWidth:'150px',
-        minHeight:'60px'
-    },
-})
+  card: {
+    maxWidth: 600,
+    margin: "auto",
+    textAlign: "center",
+    marginTop: "30px",
+    paddingBottom: "20px",
+  },
+  error: {
+    verticalAlign: "middle",
+  },
+  title: {
+    marginTop: "20px",
+    color: "red",
+  },
+  textField: {
+    marginLeft: "10px",
+    marginRight: "10px",
+    width: 300,
+  },
+  submit: {
+    margin: "0 auto !important",
+    marginBottom: "10px",
+    minWidth: "120px !important",
+    minHeight: "60px !important",
+  },
+});
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const token = useSelector(getToken);
+  const userLoginData = useSelector(getUserSigninData);
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const token = useSelector(getToken)
-    const userLoginData = useSelector(getUserSigninData)
-
-    useEffect(()=>{
-
-        if(userLoginData.hasOwnProperty('token')){
-            navigate('/dashboard')
-        }
-
-        if(token === 'Request failed with status code 500' 
-            || token ==='Request failed with status code 401'){
-            dispatch(resetStore())
-        }
-    }, [userLoginData, token.length, dispatch])
-
-    //const userSigninData = useSelector(getUserSigninData)
-    //const token = useSelector(getUserToken)
-    const classes = useStyles()
-    const [values, setValues] = useState({
-        name:'',
-        password:'',
-    })
-
-     // send request to server to login user and in case there are errors collect error
-     const clickSubmit = () => {
-        const user = {
-            name: values.name || undefined,
-            password: values.password || undefined
-        }
-        dispatch(loginUser(user))
+  useEffect(() => {
+    if (userLoginData.hasOwnProperty("token")) {
+      navigate("/dashboard");
     }
 
-    // get values from input fields
-    const handleChange = name => event => {
-        setValues({...values, [name]: event.target.value})
+    if (
+      token === "Request failed with status code 500" ||
+      token === "Request failed with status code 401"
+    ) {
+      dispatch(resetStore());
     }
+  }, [userLoginData, token.length, dispatch]);
 
-   
-    return(
-        <Grid container justifyContent='center'>
-            
-            <Grid item xs={12} md={12} lg={12} xl={12}>
+  //const userSigninData = useSelector(getUserSigninData)
+  //const token = useSelector(getUserToken)
+  const classes = useStyles();
+  const [values, setValues] = useState({
+    name: "",
+    password: "",
+  });
 
-                <Item>
-                
-                <Card className={classes.card}>
-    
-                    <CardContent>
+  // send request to server to login user and in case there are errors collect error
+  const clickSubmit = () => {
+    const user = {
+      name: values.name || undefined,
+      password: values.password || undefined,
+    };
+    dispatch(loginUser(user));
+  };
 
-                        <Typography variant='h6' className={classes.tittle}>Sign In</Typography>
+  // get values from input fields
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
 
-                        <TextField id="name" type='text' label="Username" className={classes.textField}
-                        value={values.email} onChange={handleChange('name')} margin="normal" />
-                        <br />
+  return (
+    <Grid container justifyContent="center">
+      <Grid item xs={12} md={12} lg={12} xl={12}>
+        <Item>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography variant="h6" className={classes.tittle}>
+                Sign In
+              </Typography>
 
-                        <TextField id="password" type='password' label="Password" className={classes.textField}
-                        value={values.password} onChange={handleChange('password')} margin="normal" />
-                        <br />
-            
-                        { //display error returned from server
-                            Object.keys(userLoginData).length !== 0 && (
-                                <Typography component='p' color='error'>
-                                    <Icon color='error' className={classes.error}></Icon>
-                                    {userLoginData.error}
-                                </Typography>
-                            )
-                        }
+              <TextField
+                id="name"
+                type="text"
+                label="Username"
+                className={classes.textField}
+                value={values.email}
+                onChange={handleChange("name")}
+                margin="normal"
+              />
+              <br />
 
-                    </CardContent>
+              <TextField
+                id="password"
+                type="password"
+                label="Password"
+                className={classes.textField}
+                value={values.password}
+                onChange={handleChange("password")}
+                margin="normal"
+              />
+              <br />
 
-                        <CardActions>
-                            <Button color='primary' variant="contained" onClick={clickSubmit}
-                            className={classes.submit}>Log In</Button>
-                        </CardActions>
-                </Card>
-            </Item>
-            
-        </Grid>
+              {
+                //display error returned from server
+                Object.keys(userLoginData).length !== 0 && (
+                  <Typography component="p" color="error">
+                    <Icon color="error" className={classes.error}></Icon>
+                    {userLoginData.error}
+                  </Typography>
+                )
+              }
+            </CardContent>
 
+            <CardActions>
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={clickSubmit}
+                className={classes.submit}
+              >
+                Log In
+              </Button>
+            </CardActions>
+          </Card>
+        </Item>
+      </Grid>
     </Grid>
-    )
-}
+  );
+};
 
-export default Login
+export default Login;
